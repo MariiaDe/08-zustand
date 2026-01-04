3e60f491e0bf1b69d83f8b9a5a99c839f9963152
Mariia <mariadenkovic4@gmail.com>
2026-01-04 06:29:47 +0200

    add new3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,55 +1,23 @@
 import "./globals.css";
-import type { Metadata } from "next";
-import { Roboto } from "next/font/google";
-
+import type { ReactNode } from "react";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
 
-/* =========================
-   ROBOTO FONT
-========================= */
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto",
-  display: "swap",
-});
-
-/* =========================
-   SEO METADATA
-========================= */
-export const metadata: Metadata = {
-  title: "NoteHub",
-  description:
-    "NoteHub is a simple and efficient application designed for managing personal notes.",
-  openGraph: {
-    title: "NoteHub",
-    description:
-      "NoteHub is a simple and efficient application designed for managing personal notes.",
-    url: "https://notehub.vercel.app",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({
   children,
+  modal,
 }: {
-  children: React.ReactNode;
+  children: ReactNode;
+  modal: ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>
+      <body>
         <TanStackProvider>
           <Header />
           {children}
+          {modal}
           <Footer />
         </TanStackProvider>
       </body>
